c087b4c392187a1cd339b153f1aaff5ee862ae6b
JhoanDuarte35 <johanduarte35@gmail.com>
2023-03-13 12:02:00 -0500

    cosas funcionales

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.header_login')
 
 @section('content')
 <div class="container">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,17 +12,30 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}" />
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('Puntualmente', 'Puntualmente') }}
                 </a>
+                <li class="nav-item d-none d-sm-inline-block">
+                  <a href="index3.html" class="nav-link">Home</a>
+                </li>
+              </ul> 
+
+              
+
+
+            <div class="container">
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -35,6 +48,94 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" data-toggle="dropdown" href="#">
+                              <i class="far fa-comments"></i>
+                              <span class="badge badge-danger navbar-badge">3</span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                              <a href="#" class="dropdown-item">
+                                <!-- Message Start -->
+                                <div class="media">
+                                  <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                                  <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                      Brad Diesel
+                                      <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">Call me whenever you can...</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                  </div>
+                                </div>
+                                <!-- Message End -->
+                              </a>
+                              <div class="dropdown-divider"></div>
+                              <a href="#" class="dropdown-item">
+                                <!-- Message Start -->
+                                <div class="media">
+                                  <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                                  <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                      John Pierce
+                                      <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">I got your message bro</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                  </div>
+                                </div>
+                                <!-- Message End -->
+                              </a>
+                              <div class="dropdown-divider"></div>
+                              <a href="#" class="dropdown-item">
+                                <!-- Message Start -->
+                                <div class="media">
+                                  <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                                  <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                      Nora Silvester
+                                      <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">The subject goes here</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                  </div>
+                                </div>
+                                <!-- Message End -->
+                              </a>
+                              <div class="dropdown-divider"></div>
+                              <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                            </div>
+                          </li>
+
+                          <!-- Notifications Dropdown Menu -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" data-toggle="dropdown" href="#">
+                                <i class="far fa-bell"></i>
+                                <span class="badge badge-warning navbar-badge">15</span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                                <div class="dropdown-divider"></div>
+                                <a href="#" class="dropdown-item">
+                                    <i class="fas fa-envelope mr-2"></i> 4 new messages
+                                    <span class="float-right text-muted text-sm">3 mins</span>
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a href="#" class="dropdown-item">
+                                    <i class="fas fa-users mr-2"></i> 8 friend requests
+                                    <span class="float-right text-muted text-sm">12 hours</span>
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a href="#" class="dropdown-item">
+                                    <i class="fas fa-file mr-2"></i> 3 new reports
+                                    <span class="float-right text-muted text-sm">2 days</span>
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                                </div>
+                            </li>
+                            
+
+                          
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -71,10 +172,116 @@
                 </div>
             </div>
         </nav>
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGV71hhgvdzzkhyRMVDc_GkFS2Qs2iouTLwzC2KoP_og&s" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+              <span class="brand-text font-weight-light">Puntualmente</span>
+            </a>
+        
+            <!-- Sidebar -->
+            <div class="sidebar">
+              <!-- Sidebar user panel (optional) -->
+        
+              <!-- Sidebar Menu -->
+              <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                  <!-- Add icons to the links using the .nav-icon class
+                       with font-awesome or any other icon font library -->
+                  <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                      <i class="nav-icon fas fa-tachometer-alt"></i>
+                      <p>
+                        Dashboard
+                        <i class="right fas fa-angle-left"></i>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                        <a href="./index.html" class="nav-link active">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Dashboard v1</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="./index2.html" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Dashboard v2</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Dashboard v3</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a href="pages/widgets.html" class="nav-link">
+                      <i class="nav-icon fas fa-th"></i>
+                      <p>
+                        Widgets
+                        <span class="right badge badge-danger">New</span>
+                      </p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="nav-icon fas fa-copy"></i>
+                      <p>
+                        Layout Options
+                        <i class="fas fa-angle-left right"></i>
+                        <span class="badge badge-info right">6</span>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                        <a href="pages/layout/top-nav.html" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Top Navigation</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Top Navigation + Sidebar</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="pages/layout/boxed.html" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Boxed</p>
+                        </a>
+                      </li>
+                     
+                 
+                 
+                </ul>
+              </nav>
+              <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+          </aside>
+        
+
+
+
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+        <!-- Preloader -->
+                                                {{-- configurar luego --}}
+        {{-- <div class="preloader flex-column justify-content-center align-items-center">
+          <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        </div> --}}
+      
+
+      
+
+
 </body>
 </html>
