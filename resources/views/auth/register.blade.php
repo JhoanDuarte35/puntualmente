94ae3c0f47f3ae635e1cfb1118df3a6b71614a5d
JhoanDuarte35 <johanduarte35@gmail.com>
2023-03-14 11:11:07 -0500

    melo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -94,6 +94,42 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        <div class="row mb-3">
+                            <label for="rol" class="col-md-4 col-form-label text-md-end">{{ __('Seleccione un rol') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" name="rol" id="rol" value="{{ old('telefono') }}" required autofocus>
+                                    <option value="0" selected disabled>Selecciona un Rol</option>
+                                    <option value="1">Admin</option>
+                                    <option value="2">Usuario con privilegios</option>
+                                    <option value="3">Usuario Regular</option>
+                                </select>
+                                @error('rol')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="id_empresa" class="col-md-4 col-form-label text-md-end">{{ __('Seleccione un rol') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" name="rol" id="rol" value="{{ old('telefono') }}" required autofocus>
+                                    <option value="0" selected disabled>Selecciona un Rol</option>
+                                    <option value="1">Admin</option>
+                                    <option value="2">Usuario con privilegios</option>
+                                    <option value="3">Usuario Regular</option>
+                                </select>
+                                @error('rol')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
